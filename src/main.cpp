69c06fc970c15035a3e9885d27a15223350a353f
Claus Witt <claus@wittnezz.dk>
2016-09-28 16:43:05 +0200

    adds simple autoloading of plugins

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,30 @@
+#include <dirent.h>
 #include <plugin.h>
 #include <iostream>
+#include <vector>
 #include "./plugin_handler.hpp"
+
+std::vector<PluginHandler> load_plugins() {
+  std::vector<PluginHandler> plugins;
+
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir ("plugins/bin")) != NULL) {
+    while ((ent = readdir (dir)) != NULL) {
+      if(ent->d_name[0] != '.')
+        plugins.push_back(PluginHandler("plugins/bin/" + std::string(ent->d_name)));
+    }
+    closedir (dir);
+  }
+  return plugins;
+}
 
 int main(int argc, char *argv[])
 {
-  PluginHandler first( "plugins/first/libfirst_plugin.dylib" );
-  std::cerr << "Loading plugin: " << first.get_name() << " - in version: " << first.get_version() << std::endl;
-  Plugin* plugin = first.load();
-
-  PluginHandler second( "plugins/second/libsecond_plugin.dylib" );
-  std::cerr << "Loading plugin: " << second.get_name() << " - in version: " << second.get_version() << std::endl;
-  Plugin* plugin2 = second.load();
-
-  std::cerr << plugin->command("Something", "here") << std::endl;
-  std::cerr << plugin2->command("Something", "here") << std::endl;
+  auto plugins = load_plugins();
+  for (auto ph : plugins) {
+    ph.load();
+    std::cerr << "Auto loaded plugin: " << ph.get_name() << ", version: " << ph.get_version() << std::endl;
+  }
    return 0;
 }
